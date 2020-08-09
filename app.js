fa5d52ecc92887f4d5d5fa244dfc2562d26856be
PawanPaul <pawanpauljay@gmail.com>
2020-08-09 19:09:21 +0530

    Name Num Calc
    Calculates value of name with respect to ASCII, I think

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function () {
-    'use script';
+    'use strict';
 
     angular.module('MyFirstProperApp', [])
     .controller('FirstController', function($scope){
@@ -8,6 +8,28 @@
          var greet = "It is a pleasure to meet you";
          return greet
      }
+    });
+
+})();
+(function () {
+    'use strict';
+
+    angular.module('NameCalc', [])
+    .controller('CalcCon', function($scope){
+        $scope.name = "";
+        $scope.value = 0;
+        $scope.NumDis = function(){
+            var totNV = stringnumcalc($scope.name);
+            $scope.value = totNV;
+        }
+
+        function stringnumcalc(string){
+            var val = 0;
+        for(var i=0;i<string.length;i++){
+            val += string.charCodeAt(i);
+        }
+        return val
+       }
     });
 
 })();
