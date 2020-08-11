8abf0f6fa5c6cc8d6f17bd7d2c67d019980e6b52
PawanPaul <pawanpauljay@gmail.com>
2020-08-11 22:00:04 +0530

    Fixes
    Fixing stuff

--- app2.js
+++ app2.js
@@ -3,39 +3,24 @@
 
     angular.module('LunchMenu', [])
     .controller('LunchCon', function($scope){
-        $scope.orderitem ="";
-        $scope.orderlist = [];
-        var end = 0;
-        var count = 0;
-        $scope.cusHTML = "";
-        $scope.ender = function(){
-            var thx = "";
-         if(end>0&&count<4)
-         {
-         thx = "Enjoy your meal!";
-         }
-         else if(count>3)
-         {
-             thx = "Too Much!"
-         }
-         return thx
-        }
-        $scope.end = function (){
-            end++;
-        }
+        $scope.orderitem = "";
+        $scope.output = "";
         $scope.order = function (){
             if($scope.orderitem==""){
-                $scope.cusHTML = $scope.cusHTML+"Please input data!";
+                $scope.output = "Please enter data!";
+
             }
-            else if(count<3){
-            count++;
-            $scope.orderlist[count] = $scope.orderitem;
-            $scope.cusHTML = $scope.cusHTML+count+") "+$scope.orderitem+" ";
-            }
-            else{
-                count++;
+            else {
+                let listItems = $scope.orderitem.split(',').filter(value => value.trim().length!=0);
+               if (listItems.length <= 3) {
+                $scope.output = 'Enjoy!';
+                 }
+               else {
+                $scope.output = 'Too much!';
+                 }      
             }
         }
+
     });
 
 })();
