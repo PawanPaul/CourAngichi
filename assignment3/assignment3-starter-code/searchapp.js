2eabea5b244d61108dcd684e33e57256d81f0a04
PawanPaul <pawanpauljay@gmail.com>
2020-10-12 22:29:47 +0530

    Update searchapp.js
    Small update

--- assignment3/assignment3-starter-code/searchapp.js
+++ assignment3/assignment3-starter-code/searchapp.js
@@ -7,17 +7,37 @@
     NarrowItDownController.$inject=['MenuSearchService'];
     function NarrowItDownController(MenuSearchService){
         var sysc = this;
-        sysc.found={};
+        sysc.found=[];
         var promise= MenuSearchService.getMatchedMenuItems();
+        promise.then
     }
     function MenuSearchService(){
         var service = this;
-        service.getMatchedMenuItems= function(searchTerm){
-            $http({
-                url:"https://davids-restaurant.herokuapp.com/menu_items.json"
-            }).then
+        var foundItems = [];
+        service.getMatchedMenuItems = function(searchTerm){
 
-        }
+            return  $http({
+                url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+            }).then(function(response){
+
+                var data = response.data.menu_items;
+                searchTerm = searchTerm.toLowerCase();
+
+                for(var i = 0; i < data.length; i++){
+                    
+                    var desc = data[i].description;
+                    
+                    if(desc.includes(searchTerm)){
+                        foundItems.push(data[i]);
+                    }
+                }//end of for
+
+                return foundItems;
+
+            },function(error){
+                var message = "Something's gone wrong. Try Again.";
+                return message;
+            });  
     }
 
     function foundItems({
