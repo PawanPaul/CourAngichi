206dc7c9f6a180a18eac8e4965bef107bbc8c114
PawanPaul <pawanpauljay@gmail.com>
2020-10-26 11:36:56 +0530

    Alterations
    Changing things to work slightly better

--- assignment3/assignment3-starter-code/searchapp.js
+++ assignment3/assignment3-starter-code/searchapp.js
@@ -1,49 +1,17 @@
 (function(){
     'use strict';
-    angular.module('searcher',searcher)
+    angular.module('searcher',[])
     .controller('NarrowItDownController',NarrowItDownController)
-    .service('MenuSearchService',MenuSearchService)
-    .directive('foundItems',foundItems);
-    NarrowItDownController.$inject=['MenuSearchService'];
-    function NarrowItDownController(MenuSearchService){
+    .service('MenuSearch',MenuSearchService)  
+    .directive('foundItems',foundItems)
+    function NarrowItDownController(){
         var sysc = this;
-        sysc.found=[];
-        var promise= MenuSearchService.getMatchedMenuItems();
-        promise.then
+        sysc.item = "Paul";
     }
-    function MenuSearchService(){
-        var service = this;
-        var foundItems = [];
-        service.getMatchedMenuItems = function(searchTerm){
-
-            return  $http({
-                url: "https://davids-restaurant.herokuapp.com/menu_items.json"
-            }).then(function(response){
-
-                var data = response.data.menu_items;
-                searchTerm = searchTerm.toLowerCase();
-
-                for(var i = 0; i < data.length; i++){
-                    
-                    var desc = data[i].description;
-                    
-                    if(desc.includes(searchTerm)){
-                        foundItems.push(data[i]);
-                    }
-                }//end of for
-
-                return foundItems;
-
-            },function(error){
-                var message = "Something's gone wrong. Try Again.";
-                return message;
-            });  
-    }
-
-    function foundItems({
+    function foundItems(){
         var ddo={
-            templateUrl: 'loader/itemsloaderindicator.template.html'
-        },
+            template:'Hello',
+        };
         return ddo;
-    })
+    }
 })();
